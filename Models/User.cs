01ef6b4c7a0448054c1cfb237a88fe96064bc6e1
Maddi Cook <maddihcook@gmail.com>
2023-10-23 20:01:05 -0500

    Setup

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BE_charity_direct.Models;
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string uid { get; set; }
+        public List<Charity> Charities { get; set; }
+        public List <Subscription> Subscriptions { get; set; }
+    }
+
--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
+using System.Text.Json.Serialization;
+using BE_charity_direct.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+using BE_charity_direct;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +14,34 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// allows passing datetimes without time zone data 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// allows our api endpoints to access the database through Entity Framework Core
+builder.Services.AddNpgsql<CharityDirectoryDbContext>(builder.Configuration["CharityDirectoryDbConnectionString"]);
+
+// Set the JSON serializer options
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000",
+                                "http://localhost:7287")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod()
+                                .AllowAnyOrigin();
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +55,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Create endpoints here!
 
 app.Run();
